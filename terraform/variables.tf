6e7ff3978142dbe83dc9ed03259170facd810d49
samrat2098@gmail.com <kuntaldebnath@Kuntals-MacBook-Air.local>
2026-02-12 12:09:17 +0530

    Save local changes before merge

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,13 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "kitops-eks"
+  default     = "gitops-eks"
 }
+
+## The variables below are for the VPC module. You can customize them as needed.
